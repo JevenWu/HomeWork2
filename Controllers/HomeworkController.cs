0be929cc4b0480817b2848f679df831dd6c462ff
Jeven.wu <Shining.Wu@software.dell.com>
2015-09-21 15:46:05 +0800

    Init project

---
+++ Controllers/HomeworkController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework2.Controllers
+{
+    public class HomeworkController : Controller
+    {
+        // GET: Homework
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Help()
+        {
+            return View();
+        }
+    }
+}
